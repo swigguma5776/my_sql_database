d7c2bc8013a4b4a6d47f6b1fba349d10ef6d9e46
Alex <alexrswiggum@gmail.com>
2024-04-05 11:21:12 -0500

    Added Customer Query & Insert Data

--- mysql_db.py
+++ mysql_db.py
@@ -1,32 +1,75 @@
 import mysql.connector
 
-
-
-db_name = 'ecommerce_db'
-user = 'alexs'
-password = 'alex'
-host = '127.0.0.1'
-
-try: 
-    # attempt to establish a connection
-    conn = mysql.connector.connect(
-        dabase = db_name,
-        user = user,
-        password = password,
-        host = host
-    )
-    
-    # check for successful connection
-    if conn.is_connected():
-        print(f"You have successfully connect to {db_name}")
+dict = {
+    'database' : 'ecommerce_db', # type: ignore
+    'user' : 'root', # type: ignore
+    'password' : 'Renae5776', #type: ignore
+    'host' : 'localhost' #type: ignore
+}
         
-except mysql.connector.Error as e:
-    # catch any errors
-    print(f"Error: {e}")
+        
+def customer_query():
+   
 
-finally:
-    # close the connection
-    if conn and conn.is_connected():
-        conn.close()
-        print("MySQL connection closed")
+    try: 
+        # attempt to establish a connection
+        conn = mysql.connector.connect(**dict)
+        
+        # check for successful connection
+        if conn.is_connected():
+            print(f"You have successfully connect to {dict['database']}")
+        cursor = conn.cursor()
+        
+        # starting sql query to a variable
+        query = "SELECT * FROM Customer"
+        cursor.execute(query)
+        customers = cursor.fetchall()
+        
+        for row in customers:
+            print(row)
+            
+            
+    except mysql.connector.Error as e:
+        # catch any errors
+        print(f"Error: {e}")
+
+    finally:
+        # close the connection
+        if conn and conn.is_connected():
+            conn.close()
+            print("MySQL connection closed")
+            
+
+def create_customer(*customer):
+
+
+    try: 
+        # attempt to establish a connection
+        conn = mysql.connector.connect(**dict)
+        
+        # check for successful connection
+        if conn.is_connected():
+            print(f"You have successfully connect to {dict['database']}")
+            
+        query = "INSERT INTO Customer (name, email) VALUES(%s, %s)"
+        
+        cursor = conn.cursor()
+        cursor.execute(query, customer)
+        conn.commit()
+        
+        print(f"New Customer was successfully added")
+            
+            
+    except mysql.connector.Error as e:
+        # catch any errors
+        print(f"Error: {e}")
+
+    finally:
+        # close the connection
+        if conn and conn.is_connected():
+            conn.close()
+            print("MySQL connection closed")
+    
+create_customer("Henri Swiggum", 'henris@gmail.com')  
+customer_query()   
     
